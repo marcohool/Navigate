e60a1444664478ec8ddcd35f17e05d0326acb1b7
Marco <marco.boutchelski@gmail.com>
2020-05-18 17:15:03 +0100

    New scene

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,17 @@
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
-import javax.xml.soap.Text;
-import java.awt.*;
+import java.io.IOException;
 
 public class Controller {
 
@@ -16,10 +22,21 @@ public class Controller {
     private PasswordField pwordField;
 
 
-    public void loginButtonPressed(Event event){
+    public void loginButtonPressed(Event event) throws IOException {
+
         if (unameField.getText().equals("123") && pwordField.getText().equals("123")) {
             System.out.println("cool");
-            
+
+            Parent mainParent = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+            Scene mainScene = new Scene(mainParent);
+
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            window.setScene(mainScene);
+            window.show();
+            Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+            window.setX((primScreenBounds.getWidth() - window.getWidth()) / 2);
+            window.setY((primScreenBounds.getHeight() - window.getHeight()) / 2);
+
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Verification Error");
